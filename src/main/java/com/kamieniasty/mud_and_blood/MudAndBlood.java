5841d8a35e3b53962c0b84fd7f429c844d35b954
kamieniasty <piotrzielinski6262@gmail.com>
2024-05-11 00:09:37 +0200

    Initial commit

---
+++ src/main/java/com/kamieniasty/mud_and_blood/MudAndBlood.java
@@ -0,0 +1,26 @@
+package com.kamieniasty.mud_and_blood;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(MudAndBlood.MOD_ID)
+public class MudAndBlood {
+    public static final String MOD_ID = "mud_and_blood";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public MudAndBlood() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
